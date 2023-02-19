13ec0b1e19f95c16c3baf6eae008e6b586439832
Brian Kelly <brian.kelly.tech@gmail.com>
2023-02-19 13:23:47 -0500

    Created file structure, node.js server, and gitignore for node modules.

---
+++ server.js
@@ -0,0 +1,20 @@
+// import expresss and path library
+const express = require('express');
+const path = require('path');
+
+//store public folder path in a separate variable
+let initial_path = path.join(__dirname, "public");
+
+let app = express();
+app.use(express.static(initial_path));
+
+// setup home route to send index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initial_path, "index.html"));
+})
+
+// local port where app will display
+app.listen(5500, () => {
+    console.log('Server is listening on port 5500...')
+})
+
